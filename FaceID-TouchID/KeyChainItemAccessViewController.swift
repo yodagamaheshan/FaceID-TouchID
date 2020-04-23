6c5f95363431917bc5e139b0abc2beea2866881c
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-04-23 21:51:24 +0530

    create SecAccessControlCreateWithFlags instance

--- FaceID-TouchID/KeyChainItemAccessViewController.swift
+++ FaceID-TouchID/KeyChainItemAccessViewController.swift
@@ -21,14 +21,11 @@ class KeyChainItemAccessViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addCredentials(_ credentials: Credentials, server: String) {
+        let accsess = SecAccessControlCreateWithFlags(nil,//use default allocator
+            kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
+            .userPresence,
+            nil)//ignore any error
     }
 
 }
